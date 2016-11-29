8342225d115604c44d924dfe8c3a8f0d51bdf024
Anto Tomas <at@MRWS>
2016-11-29 09:24:06 +0100

    Edited the labels (text value) and textboxes (size)

--- WF app 1 - Git testing/Form1.Designer.cs
+++ WF app 1 - Git testing/Form1.Designer.cs
@@ -53,13 +53,13 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Name";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(53, 6);
+            this.textBox1.Location = new System.Drawing.Point(83, 6);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
             this.textBox1.TabIndex = 2;
             // 
             // label2
@@ -67,15 +67,15 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Surname";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(53, 40);
+            this.textBox2.Location = new System.Drawing.Point(83, 40);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(162, 20);
             this.textBox2.TabIndex = 4;
             // 
             // label3
@@ -83,15 +83,15 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 77);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(65, 13);
             this.label3.TabIndex = 5;
-            this.label3.Text = "label3";
+            this.label3.Text = "Date of birth";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(53, 74);
+            this.textBox3.Location = new System.Drawing.Point(83, 74);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(162, 20);
             this.textBox3.TabIndex = 6;
             // 
             // Form1
