f60d4730f6b7d07ca486c1c9238964002d94aca8
Anto Tomas <at@MRWS>
2016-11-29 10:21:50 +0100

    Added another class 2

---
+++ WF app 1 - Git testing/AnotherClass2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_app_1___Git_testing
+{
+    class AnotherClass2
+    {
+        /*
+         * Just for testing the stageing commit
+        */
+        public int someMethod2(int number)
+        {
+            int someNumber = number;
+            return someNumber;
+        }
+
+        public void otherMethor2()
+        {
+            //nothing yet
+        }
+    }
+}
