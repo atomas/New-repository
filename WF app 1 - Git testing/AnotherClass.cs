ef4ab952a88dfc104461558350aa788396d1a4dc
Anto Tomas <at@MRWS>
2016-11-29 09:41:57 +0100

    Added a new class

---
+++ WF app 1 - Git testing/AnotherClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_app_1___Git_testing
+{
+    class AnotherClass
+    {
+        /*
+         * Just for testing the stageing commit
+        */
+    }
+}
